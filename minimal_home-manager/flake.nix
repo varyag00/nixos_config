2897b625efba74ae8a8f11029a3d27e79ddae7db
Dan Gonzalez <jdgonzal@proton.me>
2024-04-12 21:45:43 +0200

    Add minimal flake w/ home-manager

---
+++ minimal_home-manager/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/nixos-wsl";
+
+    home-manager = {
+      # url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        wsl = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            nixos-wsl.nixosModules.wsl
+          ];
+        };
+      };
+      homeConfigurations = {
+        dan = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          inherit system;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
