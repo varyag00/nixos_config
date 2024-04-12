9ec6c06d631de6b47735a3fd000c0ae0864bdf9e
Dan G <jdgonzal@proton.me>
2024-04-12 11:22:53 +0000

    Added system packages

--- dan/config.nix
+++ dan/config.nix
@@ -1,6 +1,20 @@
 { config, pkgs, ... }: {
 
-  environment.systemPackages = with pkgs; [ neovim git wget ];
+  environment.systemPackages = with pkgs; 
+  [ 
+    zsh
+    atuin
+    neovim 
+    git 
+    wget
+    ripgrep
+    lsd
+    fzf
+    direnv
+    bat
+    dog
+    zoxide
+  ];
 
   # enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
