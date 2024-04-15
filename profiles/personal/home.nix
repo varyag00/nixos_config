a841c853486c0495d044e4170de8dd2012397306
Dan Gonzalez <jdgonzal@proton.me>
2024-04-15 12:30:48 +0200

    home-manager: use patched vscode server

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -5,17 +5,19 @@
   home.homeDirectory = "/home/"+userSettings.username;
 
   programs.home-manager.enable = true;
-
-  imports = [
-    ../../user/shell/sh.nix # shell
-    # ../../user/shell/cli-collection.nix # Useful CLI apps
-  ];
-
-  home.stateVersion = "23.11"; # Please read the comment before changing.
-
   # home.packages = with pkgs; [
   # git
   # zsh
   # wezterm
   # ];
+
+  imports = [
+    ../../user/shell/sh.nix # shell
+    # ../../user/shell/cli-collection.nix # Useful CLI apps
+    "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+  ];
+  services.vscode-server.enable = true;
+
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
 }
