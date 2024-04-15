b8034e55c022dbb912b9f31a85aa2d1db8a39e7a
Dan Gonzalez <jdgonzal@proton.me>
2024-04-15 13:19:23 +0200

    Use nix-ld-rs link vscode-server

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -5,20 +5,30 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-
-  wsl.enable = true;
-  # NOTE: updated from default "NixOS", and the ran the commands documented at url below
-  # DOC: https://github.com/nix-community/NixOS-WSL/pull/406/files/379af73917816dd3d153b5862e648df3ba77ad32
-  wsl.defaultUser = "dan";
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  wsl = {
+    enable = true;
+    # NOTE: updated from default "NixOS", and the ran the commands documented at url below
+    # DOC: https://github.com/nix-community/NixOS-WSL/pull/406/files/379af73917816dd3d153b5862e648df3ba77ad32
+    defaultUser = "dan";
+
+    # extraBin = with pkgs; [
+    #   { src = "${coreutils}/bin/uname"; }
+    #   { src = "${coreutils}/bin/dirname"; }
+    #   { src = "${coreutils}/bin/readlink"; }
+    # ];
+  };
+
   # allow dynamically linked programs to be loaded
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    # Add any missing dynamic libraries for unpackaged programs
-    # here, NOT in environment.systemPackages
-  ];
+  programs.nix-ld = {
+    enable = true;
+    # libraries = with pkgs; [
+    #   # Add any missing dynamic libraries for unpackaged programs
+    #   # here, NOT in environment.systemPackages
+    # ];
+    package = inputs.nix-ld-rs.packages."${pkgs.system}".nix-ld-rs;
+  };
 
 
   time.timeZone = "Europe/Amsterdam";
@@ -44,6 +54,22 @@
     bat
     dog
     zoxide
+  ];
+
+  # SECTION: services
+
+  services.openssh = {
+    enable = true;
+    # require public key authentication for better security
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+    #settings.PermitRootLogin = "yes";
+  };
+  users.users."user".openssh.authorizedKeys.keys = [
+    # dan-wks-3080 windows key
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIdjUmRaJCKDBRZHI6VyKKOOdWD08Ezg5Aqa+u3cL8ZNdAzPudikv5x6RPemcjWg4Pj0s8nXsFl9UYJmDW7tgCdaf6m20aWu/0R3tGjOc+O+MfGnGkbdvH0gl9gm7OtGeywn1BO777SlmXnu788+69DLcXftjgf4za/AW3mP/LnPPp2TKgONi/+4nKQSC/20H0yAZib7u4cav4QBHTy2u7UvmDLHKPGfP4OwINVVub2LI+bzMrbTqs2LrZzG9JyfdNTojZh6lszubkVQ9cNojsWcmovn2iswruTgtjvzxeENEWHk6VdJUKr1bSDusIQ0ucDTuqbJqA80bP9l4m+GqSZfTMjNC+m/gljSW33oDmkiXgW5VZb6RZV3gktqngDT8ghfkFkHi3JfRtGy1THWEOskGz+fGQ5w9j9Q9tB9WBGqfMxE0u6P/65a+bnmypntGv649RpxD3nJ7e7FwPzy9Ekcoy7IZffDuoTvqbqjIcAfyHOT9iLFPg233KuYMxGi0= dgonz@dan-wks-3080"
+    # note: ssh-copy-id will add user@your-machine after the public key
+    # but we can remove the "@your-machine" part
   ];
 
   # Configure network proxy if necessary
