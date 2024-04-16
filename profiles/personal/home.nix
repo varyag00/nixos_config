9e53c9eb2fcc2ca9e2c93d7ca16d62db404976f8
Dan G <jdgonzal@proton.me>
2024-04-16 21:01:57 +0200

    temp

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -14,9 +14,9 @@
   imports = [
     ../../user/shell/sh.nix # shell
     # ../../user/shell/cli-collection.nix # Useful CLI apps
-    "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+    #"${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
   ];
-  services.vscode-server.enable = true;
+  #services.vscode-server.enable = true;
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
