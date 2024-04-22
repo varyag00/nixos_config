a16668aad5b445981e9a338eeb361e484cfa472e
Dan Gonzalez <jdgonzal@proton.me>
2024-04-22 14:52:27 +0200

    Delete old/ dir to forget my past hubris

--- old/dan/nix_aliases.sh
+++
@@ -1,8 +0,0 @@
-alias vi="nvim"
-
-# default rebuild
-alias nix_rb="sudo nixos-rebuild switch"
-
-# rebuild using /etc/nixos/flake.nix (default)
-# requires enabling flakes in /etc/nixos/dan_conf.nix
-alias nix_rbf="sudo nixos-rebuild switch --flake ."
